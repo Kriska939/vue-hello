0897074c6089a95bca693bd31c58f4dcca8ccd32
Kriska939 <cristinacorrado93@gmail.com>
2021-07-27 15:45:40 +0200

    added basic files + basic exercise complete

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* Esercizio: 
+Inizializzare Vue e stampare a schermo un messaggio all’interno di un h1, utilizzando le properietà di Vue presenti in data.
+
+BONUS: 
+
+Aggiungere alla pagina un’immagine, la cui sorgente derivi anch'essa dai data di Vue.
+*/
+
+
+// check:
+// console.log("Vue is working", Vue);
+
+
+const app = new Vue({
+    el: "#root",
+    data: {
+        message: "Hello, Vue!"
+    }
+}
+
+);
